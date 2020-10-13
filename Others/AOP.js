439b5807f133c5d373ad4281c79d8aaf2b252885
Frank Lai <frank810618@gmail.com>
2020-10-13 10:03:57 +0800

    add comments for AOP

--- Others/AOP.js
+++ Others/AOP.js
@@ -1,16 +1,16 @@
 Function.prototype.before = function (beforeFn) {
-    let _self = this;
-    return function () {
-        beforeFn.apply(null, arguments);
-        return _self.apply(null, arguments);
+    let _self = this; // use _self to keep this reference, which points to the function
+    return function () { // a new function wrapper
+        beforeFn.apply(null, arguments); // trigger registered before behavior
+        return _self.apply(null, arguments); // trigger function itself
     };
 }
 
 Function.prototype.after = function (afterFn) {
     let _self = this;
     return function () {
-        var result = _self.apply(null, arguments);
-        afterFn.apply(null, arguments);
+        var result = _self.apply(null, arguments); // trigger function and record result
+        afterFn.apply(null, arguments); // trigger registered after behavior
         return result;
     };
 }
@@ -19,7 +19,7 @@ function func() {
     console.log('content');
 }
 
-func = func.before(function () {
+func = func.before(function () { // register before and after actions
     console.log('before');
 }).after(function () {
     console.log('after');
