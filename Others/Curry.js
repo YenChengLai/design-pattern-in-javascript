d19041c4714359c6d60243c2051e6e9bbe0cd399
Frank Lai <frank810618@gmail.com>
2020-10-16 11:03:20 +0800

    add after curried code

--- Others/Curry.js
+++ Others/Curry.js
@@ -1,12 +1,35 @@
 // Before Currying
-let dailyCost = 0;
+// let dailyCost = 0;
 
-function addCost (cost) {
-    dailyCost += cost;
+// function addCost(cost) {
+//     dailyCost += cost;
+// }
+
+// addCost()(10);
+// addCost()(20);
+// addCost()(30);
+// addCost()();
+
+// console.log(dailyCost);
+
+
+// After Currying
+// put arguments into function cache but not calculate on the fly
+// calculate the total in the cache if no arguments passed
+function addCostCurried() {
+    const costArr = [];
+    return function () {
+        if (arguments.length == 0) {
+            return costArr.reduce((acc, curr) => acc + curr, 0);
+        }
+        costArr.push(parseInt(arguments[0]));
+    }
 }
 
-addCost(10);
-addCost(20);
-addCost(30);
+const count = addCostCurried();
 
-console.log(dailyCost);
+count(5);
+count(10);
+count(15);
+
+console.log(count());
