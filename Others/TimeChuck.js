8f156d29bffb2233a02b5034a6bc774df02dbd01
Frank Lai <frank810618@gmail.com>
2020-10-21 22:05:19 +0800

    optimize and add comment

--- Others/TimeChuck.js
+++ Others/TimeChuck.js
@@ -1,9 +1,16 @@
-function TimeChuck(array, fn, count) {
+/**
+ * Separate repeating function executions into small chucks
+ * @param {Array} array Data that will be executed.
+ * @param {Function} fn The real working function.
+ * @param {number} count The max amount of the executions that will be triggered in the same time.
+ * @param {number} interval The interval of each execution.
+ */
+function TimeChuck(array, fn, count, interval) {
     let obj, timer;
 
-    function start() {
+    function start() { // this function executes the passing function
         for (let i = 0; i < Math.min(count || 1, array.length); i++) {
-            obj = array.shift();
+            obj = array.shift(); // get the first element and run the function
             fn(obj);
         }
         console.log('-------------------------');
@@ -12,12 +19,12 @@ function TimeChuck(array, fn, count) {
     return function () {
         timer = setInterval(() => {
             if (array.length === 0) {
-                return clearInterval(timer);
+                return clearInterval(timer); // clear the interval when all tasks done
             }
             start();
-        }, 2000);
+        }, interval);
     }
 }
 
-let printNumberEach2Sec = TimeChuck([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], console.log, 2);
+let printNumberEach2Sec = TimeChuck([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], console.log, 5, 2000);
 printNumberEach2Sec();
