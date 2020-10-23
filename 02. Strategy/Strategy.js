83fbc217ef4ad1a623cdbced1af940a762d92426
Frank Lai <frank810618@gmail.com>
2020-10-23 14:24:57 +0800

    Use Strategy Pattern in classical inheritance

--- 02. Strategy/Strategy.js
+++ 02. Strategy/Strategy.js
@@ -1,14 +1,59 @@
 // Normal Solution
-function getBonus(performanceLevel, salary) {
-    if (performanceLevel == '5')
-        return salary * 5;
-    else if (performanceLevel == '4')
-        return salary * 4;
-    else if (performanceLevel == '3')
-        return salary * 3;
-    else
-        return salary * 1;
+// function getBonus(performanceLevel, salary) {
+//     if (performanceLevel == '5')
+//         return salary * 5;
+//     else if (performanceLevel == '4')
+//         return salary * 4;
+//     else if (performanceLevel == '3')
+//         return salary * 3;
+//     else
+//         return salary * 1;
+// }
+
+// console.log(getBonus('4', 40000));
+// console.log(getBonus('5', 45000));
+
+// Strategy Pattern in classical inheritance
+// Below are the strategy functions
+function level3() { };
+level3.prototype.getBonus = function (salary) {
+    return salary * 3;
 }
 
-console.log(getBonus('4', 40000));
-console.log(getBonus('5', 45000));
+function level4() { };
+level4.prototype.getBonus = function (salary) {
+    return salary * 4;
+}
+
+function level5() { };
+level5.prototype.getBonus = function (salary) {
+    return salary * 5;
+}
+
+// Below are the Context Object to be called by users
+function Bonus() {
+    this.salary = null;
+    this.strategy = null;
+};
+
+Bonus.prototype.setSalary = function (salary) {
+    this.salary = salary;
+}
+
+Bonus.prototype.setStrategy = function (strategy) {
+    this.strategy = strategy;
+}
+
+Bonus.prototype.getBonus = function () {
+    return this.strategy.getBonus(this.salary);
+}
+
+// use case
+var bonusJimmy = new Bonus();
+bonusJimmy.setSalary(48000);
+bonusJimmy.setStrategy(new level4());
+console.log(bonusJimmy.getBonus());
+
+bonusJimmy.setStrategy(new level5());
+console.log(bonusJimmy.getBonus());
+
