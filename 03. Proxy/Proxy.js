ee55c5844401709bb31aff601d707ee44a8a39e5
Frank Lai <frank810618@gmail.com>
2020-10-25 23:09:27 +0800

    revise multiply function

--- 03. Proxy/Proxy.js
+++ 03. Proxy/Proxy.js
@@ -1,6 +1,10 @@
 function multiply() {
     console.log('start');
-    return arguments.reduce((x, y) => x * y);
+    let total = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+    }
+    return total;
 }
 
 console.log(multiply(2, 3));
