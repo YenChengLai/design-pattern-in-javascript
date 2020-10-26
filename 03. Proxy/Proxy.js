b1a0991a82df89379214fe82ef9a55a96abab90f
Frank Lai <frank810618@gmail.com>
2020-10-26 22:20:30 +0800

    adjust proxy with factory design

--- 03. Proxy/Proxy.js
+++ 03. Proxy/Proxy.js
@@ -1,6 +1,16 @@
 // Multiply Function
 function multiply() {
-    console.log('start');
+    let total = 1;
+    for (let i = 0; i < arguments.length; i++) {
+        total *= arguments[i];
+    }
+    return total;
+}
+
+console.log(multiply(2, 3));
+
+// Plus Function
+function plus() {
     let total = 0;
     for (let i = 0; i < arguments.length; i++) {
         total += arguments[i];
@@ -8,20 +18,23 @@ function multiply() {
     return total;
 }
 
-console.log(multiply(2, 3));
-
-// Cache Proxy
-let ProxyMultiply = (function () {
+// Cache Proxy with Factory design (Remove IIFE)
+let proxyCacheFactory = function (fn) {
     let cache = {};
     return function () {
         let args = Array.prototype.join.call(arguments, ','); // Stored as key in an object
         if (args in cache) {
             return cache[args];
         }
-        return cache[args] = multiply.apply(this, arguments);
+        return cache[args] = fn.apply(this, arguments); // trigger function and store result
     }
-})();
+};
+
+// use curry function to create functions
+let proxyMultiply = proxyCacheFactory(multiply),
+    proxyPlus = proxyCacheFactory(plus);
 
 // Same operation will be handled by cache
-console.log(ProxyMultiply(3, 5));
-console.log(ProxyMultiply(3, 5));
+console.log(proxyMultiply(3, 5));
+console.log(proxyPlus(3, 5));
+
