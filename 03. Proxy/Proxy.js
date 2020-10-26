f9a5f4e762c0c3e500e79463a44dc1aa87cf7e9c
Frank Lai <frank810618@gmail.com>
2020-10-26 22:10:11 +0800

    add Proxy Multiply function

--- 03. Proxy/Proxy.js
+++ 03. Proxy/Proxy.js
@@ -1,3 +1,4 @@
+// Multiply Function
 function multiply() {
     console.log('start');
     let total = 0;
@@ -8,3 +9,19 @@ function multiply() {
 }
 
 console.log(multiply(2, 3));
+
+// Cache Proxy
+let ProxyMultiply = (function () {
+    let cache = {};
+    return function () {
+        let args = Array.prototype.join.call(arguments, ','); // Stored as key in an object
+        if (args in cache) {
+            return cache[args];
+        }
+        return cache[args] = multiply.apply(this, arguments);
+    }
+})();
+
+// Same operation will be handled by cache
+console.log(ProxyMultiply(3, 5));
+console.log(ProxyMultiply(3, 5));
