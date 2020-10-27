d40b78ff6e30ac2a5747da00fa638cb39a70b91b
Frank Lai <frank810618@gmail.com>
2020-10-27 16:13:58 +0800

    add outer Iterator

--- 04. Iterator/Iterator.js
+++ 04. Iterator/Iterator.js
@@ -8,3 +8,25 @@ function each(array, callback) {
 each([1, 2, 3], function (obj) {
     console.log(obj);
 });
+
+// Outer Iterator
+function Iterator(object) {
+    let current = 0;
+
+    return {
+        next: function () {
+            current++;
+        },
+        isDone: function () {
+            return current >= object.length;
+        },
+        getCurrentItem: function () {
+            return object[current];
+        }
+    }
+}
+
+let iter = Iterator([1, 2, 3]);
+console.log(iter.next());
+console.log(iter.isDone());
+console.log(iter.getCurrentItem());
