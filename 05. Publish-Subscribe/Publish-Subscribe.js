eac43f3e8f902dd48fc92876ae7b1b5ab47929a0
Frank Lai <frank810618@gmail.com>
2020-11-06 16:49:51 +0800

    add comments and test case

--- 05. Publish-Subscribe/Publish-Subscribe.js
+++ 05. Publish-Subscribe/Publish-Subscribe.js
@@ -1,14 +1,26 @@
-
+// Observer Object
 const observer = {};
 
+// Array to Store all listened functions
 observer.listenArray = [];
 
+// listener
 observer.listen = function (fn) {
     this.listenArray.push(fn);
 }
 
+// trigger all listened function with arguments passed in
 observer.trigger = function () {
     for (let i = 0; i < this.listenArray.length; i++) {
         this.listenArray[i].apply(null, arguments);
     }
 }
+
+// test cases
+let sayHi = name => console.log(`Hi, ${name}!`);
+let sayHello = name => console.log(`Hello, ${name}!`);
+
+observer.listen(sayHi);
+observer.listen(sayHello);
+
+observer.trigger('John');
