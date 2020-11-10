30cb33017eb9ef38d3fe9527e59ae3b397eec0c6
Frank Lai <frank810618@gmail.com>
2020-11-10 23:38:25 +0800

    add basic mapping settings

---
+++ 06. Command/Command.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    const button1 = document.getElementById('button1')
+    const button2 = document.getElementById('button2')
+    const button3 = document.getElementById('button3')
+}
