fd6907daf2abba94d3eadb18823fba1e50fe691f
Frank Lai <frank810618@gmail.com>
2020-11-17 23:58:01 +0800

    minor adjust

--- 06. Command/Command.js
+++ 06. Command/Command.js
@@ -1,5 +1,7 @@
 window.onload = function () {
-    const button1 = document.getElementById('button1')
-    const button2 = document.getElementById('button2')
-    const button3 = document.getElementById('button3')
+    const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
+    const button3 = document.getElementById('button3');
+
+
 }
