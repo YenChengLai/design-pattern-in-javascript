2014380214a90e1e153567040aa9d124d8ae5d64
Frank Lai <frank810618@gmail.com>
2020-11-25 16:46:13 +0800

    add setCommand function

--- 06. Command/Command.js
+++ 06. Command/Command.js
@@ -3,4 +3,15 @@ window.onload = function () {
     const button2 = document.getElementById('button2');
     const button3 = document.getElementById('button3');
 
+    // set command: register passed command as click event onto passed button
+    const setCommand = function (button, command) {
+        button.onclick = function () {
+            command();
+        }
+    }
+
+    // use case
+    setCommand(button1, function () { console.log('button 1 clicked'); });
+    setCommand(button2, function () { console.log('button 2 clicked'); });
+    setCommand(button3, function () { console.log('button 3 clicked'); });
 }
